fbe42a5835f69bb26b605c1f0c18ea24848906d4
OlivierParent <olivier.parent@arteveldehs.be>
2019-05-08 18:41:09 +0200

    [WIP]

--- big-o/main.js
+++ big-o/main.js
@@ -1,107 +1,107 @@
 /**
  * O(1)
- * 
+ *
  * The complexity is always the same, and therefore maximum 1 × time necessary to complete.
- * 
- * @param {int} n 
+ *
+ * @param {int} n
  */
-function once(n) {
-    let iteration = 1;
-    for (let i = 0; i < 9; i++) {
-        console.log('iteration', `${iteration++}:`, 'i is', i, ', n is', n);
-    }
+function once (n) {
+  let iteration = 1
+  for (let i = 0; i < 9; i++) {
+    console.log('iteration', `${iteration++}:`, 'i is', i, ', n is', n)
+  }
 }
 
 /**
  * O(∞)
- * 
+ *
  * The complexity keeps increasing.
- * 
- * @param {int} n 
+ *
+ * @param {int} n
  */
-function endless(n) {
-    let iteration = 1;
-    while (true) {
-        console.log('iteration', `${iteration++}:`, 'n is', n);
-    }
+function endless (n) {
+  let iteration = 1
+  while (true) {
+    console.log('iteration', `${iteration++}:`, 'n is', n)
+  }
 }
 
 /**
  * O(n)
- * 
+ *
  * The complexity is n × time necessary to complete.
  *
- * @param {int} n 
+ * @param {int} n
  */
-function linear(n) {
-    let iteration = 1;
-    for (let i = 0; i < n; i++) {
-        console.log('iteration', `${iteration++}:`, 'i is', i, '; n is', n);
-    }
+function linear (n) {
+  let iteration = 1
+  for (let i = 0; i < n; i++) {
+    console.log('iteration', `${iteration++}:`, 'i is', i, '; n is', n)
+  }
 }
 
 /**
  * O(n^2)
- * 
- * @param {int} n 
+ *
+ * @param {int} n
  */
-function quadratic(n) {
-    let iteration = 1;
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            console.log('iteration', `${iteration++}:`, 'i is', i, '; j is', j, '; n is', n);
-        }
+function quadratic (n) {
+  let iteration = 1
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log('iteration', `${iteration++}:`, 'i is', i, '; j is', j, '; n is', n)
     }
+  }
 }
 
 /**
  * O(n^3)
- * 
- * @param {int} n 
+ *
+ * @param {int} n
  */
-function cubic(n) {
-    let iteration = 1;
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            for (let k = 0; k < n; k++) {
-                console.log('iteration', `${iteration++}:`, 'i is', i, '; j is', j, '; k is', k, '; n is', n);
-            }
-        }
+function cubic (n) {
+  let iteration = 1
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      for (let k = 0; k < n; k++) {
+        console.log('iteration', `${iteration++}:`, 'i is', i, '; j is', j, '; k is', k, '; n is', n)
+      }
     }
+  }
 }
 
 /**
  * O(n^n)
- * 
- * @param {int} n 
+ *
+ * @param {int} n
  */
-function polynomialN(n) {
-    let iteration = 1;
-    for (let i = 0; i < n ** n; i++) {
-        console.log('iteration', `${iteration++}:`, 'i is', i, '; n is', n);
-    }
+function polynomialN (n) {
+  let iteration = 1
+  for (let i = 0; i < n ** n; i++) {
+    console.log('iteration', `${iteration++}:`, 'i is', i, '; n is', n)
+  }
 }
 
 /**
  * O(log2(n))
- * 
- * @param {int} n 
+ *
+ * @param {int} n
  */
-function logarithmic2(n) {
-    const base = 2;
-    for (let iteration = 1, i = base; i <= n; iteration++, i *= base) {
-        console.log('iteration', `${iteration}:`, 'i is', i, '; n is', n);
-    }
+function logarithmic2 (n) {
+  const base = 2
+  for (let iteration = 1, i = base; i <= n; iteration++, i *= base) {
+    console.log('iteration', `${iteration}:`, 'i is', i, '; n is', n)
+  }
 }
 
 /**
  *  O(log10(n))
- * 
- * @param {int} n 
+ *
+ * @param {int} n
  */
-function logarithmic10(n) {
-    const base = 10;
-    for (let iteration = 1, i = base; i <= n; iteration++, i *= base) {
-        console.log('iteration', `${iteration}:`, 'i is', i, '; n is', n);
-    }
+function logarithmic10 (n) {
+  const base = 10
+  for (let iteration = 1, i = base; i <= n; iteration++, i *= base) {
+    console.log('iteration', `${iteration}:`, 'i is', i, '; n is', n)
+  }
 }
--- tfjs-image-classification/assets/js/app.js
+++ tfjs-image-classification/assets/js/app.js
@@ -1,54 +1,54 @@
 window.onload = function () {
-    start()
+  start()
 }
 
 let image, imageAnalysisLabelsElement, imageAnalysisLoaderElement, imagePreviewElement
 
-function start() {
-    image = new Image()
-    imageAnalysisLabelsElement = document.getElementById('image-analysis-labels')
-    imageAnalysisLoaderElement = document.getElementById('image-analysis-loader')
-    imagePreviewElement = document.getElementById('image-preview')
-    document.getElementById('image-input').addEventListener('change', imageInputChangedHandler)
+function start () {
+  image = new Image()
+  imageAnalysisLabelsElement = document.getElementById('image-analysis-labels')
+  imageAnalysisLoaderElement = document.getElementById('image-analysis-loader')
+  imagePreviewElement = document.getElementById('image-preview')
+  document.getElementById('image-input').addEventListener('change', imageInputChangedHandler)
 }
 
-function imageInputChangedHandler(event) {
-    image.src = URL.createObjectURL(event.target.files[0])
-    imagePreviewElement.src = image.src
-    imageAnalysisLoaderElement.style.display = 'block'
-    imageAnalysisLabelsElement.innerHTML = ''
-    detect()
+function imageInputChangedHandler (event) {
+  image.src = URL.createObjectURL(event.target.files[0])
+  imagePreviewElement.src = image.src
+  imageAnalysisLoaderElement.style.display = 'block'
+  imageAnalysisLabelsElement.innerHTML = ''
+  detect()
 }
 
-async function detect() {
-    const VERSION = 2
-    const ALPHA = .75
-    const model = await mobilenet.load(VERSION, ALPHA)
+async function detect () {
+  const VERSION = 2
+  const ALPHA = 0.75
+  const model = await mobilenet.load(VERSION, ALPHA)
 
-    const predictions = await model.classify(image)
-    showPredictions(predictions)
+  const predictions = await model.classify(image)
+  showPredictions(predictions)
 
-    console.log('Logits:')
-    const logits = model.infer(image)
-    logits.print(true)
+  console.log('Logits:')
+  const logits = model.infer(image)
+  logits.print(true)
 
-    console.log('Embedding:')
-    const embedding = model.infer(image, true)
-    embedding.print(true)
+  console.log('Embedding:')
+  const embedding = model.infer(image, true)
+  embedding.print(true)
 }
 
-function showPredictions(predictions) {
-    imageAnalysisLoaderElement.style.display = 'none'
-    const liClass = [
-        'mt-2'
-    ].join(' ')
-    const spanClass = [
-        'align-middle',
-        'bg-gray-600',
-        'px-2',
-        'py-1',
-        'rounded-full',
-        'text-white',
-    ].join(' ')
-    imageAnalysisLabelsElement.innerHTML = predictions.map(prediction => `<li class="${liClass}"><span class="${spanClass}">${Math.round(prediction.probability * 100)}%</span> ${prediction.className}</li>`).join('\n')
+function showPredictions (predictions) {
+  imageAnalysisLoaderElement.style.display = 'none'
+  const liClass = [
+    'mt-2'
+  ].join(' ')
+  const spanClass = [
+    'align-middle',
+    'bg-gray-600',
+    'px-2',
+    'py-1',
+    'rounded-full',
+    'text-white'
+  ].join(' ')
+  imageAnalysisLabelsElement.innerHTML = predictions.map(prediction => `<li class="${liClass}"><span class="${spanClass}">${Math.round(prediction.probability * 100)}%</span> ${prediction.className}</li>`).join('\n')
 }
